a532300becc1348d342f3e3df64bf636f834f29f
Wesley MacLaggan <wmaclaggan@gmail.com>
2015-10-15 05:06:18 +0000

    From file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
+var buffer = fs.readFileSync('index.html','utf-8')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2')
+  response.send(buffer)
 })
 
 app.listen(app.get('port'), function() {
